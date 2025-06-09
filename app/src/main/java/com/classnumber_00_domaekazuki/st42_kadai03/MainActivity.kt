2970b69b62a4dc0549ae3fef9b284988f46d0e1a
haldomae <hal.domae@gmail.com>
2025-06-09 21:31:10 +0900

    結果表示テキストを追加

--- app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai03/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai03/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -83,6 +84,17 @@ private fun MainContent(){
                 fontWeight = FontWeight.Bold
             )
         }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // 結果表示テキスト
+        Text(
+            text = "あなたのBMIは00.0です",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = Color.Gray,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
     }
 }
 
