7c96c4459fd3bb87cc31e0ba12105df5cc3687e5
haldomae <hal.domae@gmail.com>
2025-06-12 10:38:50 +0900

    テキストを入力できるように修正

--- app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai03/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai03/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,21 +23,25 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import com.classnumber_00_domaekazuki.st42_kadai03.ui.theme.ST42_kadai03Theme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ST42_kadai03Theme {
-                MainContent()
+                MainContent(
+                    viewModel = viewModel
+                )
             }
         }
     }
 }
 @Composable
-fun MainContent(){
+fun MainContent(viewModel: MainViewModel){
     Column(
         // 全体余白
         modifier = Modifier.padding(20.dp),
@@ -56,15 +61,17 @@ fun MainContent(){
                 Text(text = "170cm")
             },
             // テキストに表示されている文字列
-            value = "",
+            value = viewModel.num,
             // 内容が変わった時に実行される
-            onValueChange = {},
+            onValueChange = {
+                viewModel.num = it
+            },
             colors = TextFieldDefaults.colors(
                 // フォーカスされていないときの色
                 unfocusedContainerColor = Color.Transparent,
                 // フォーカルされている時の色
                 focusedContainerColor = Color.Transparent
-            ),
+            )
         )
     }
 
